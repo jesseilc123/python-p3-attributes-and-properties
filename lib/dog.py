99521c303970820a1d0c23196c0cc12bd5bb4429
Jesse Ilc <jesseilc123@gmail.com>
2023-05-22 13:11:06 -0500

    Lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Mastiff"):
+        if (type(name) != int) and 0 < len(name) < 26:
+            name.title()
+            self.name = name 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if breed in APPROVED_BREEDS:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+        name = property(name)
+        breed = property(breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,17 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Name", job="Admin"):
+        if (type(name) != int) and 0 < len(name) < 26:
+            newName = name.title()
+            self.name = newName 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if job in APPROVED_JOBS:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+        name = property(name)
+        job = property(job)
